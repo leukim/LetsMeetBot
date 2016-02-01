ee538fcbc733701a9668ef017341eb44e8d88686
Miquel Puig i Pey <miquelpuigpey@gmail.com>
2016-02-01 22:16:47 +0100

    #1 Moved DB usage to an interface. Implemented DB in SQLite.

--- src/test/java/com/leukim/lmb/Main.java
+++ src/test/java/com/leukim/lmb/Main.java
@@ -3,8 +3,6 @@ package com.leukim.lmb;
 import org.telegram.telegrambots.TelegramApiException;
 import org.telegram.telegrambots.TelegramBotsApi;
 
-import java.io.IOException;
-
 /**
  * Main class. Used as entry point.
  *
@@ -15,11 +13,8 @@ public class Main {
     public static void main(String[] args) {
         TelegramBotsApi api = new TelegramBotsApi();
         try {
-            LetsMeetBot letsMeetBot = new LetsMeetBot("src/main/resources/events.txt");
+            LetsMeetBot letsMeetBot = new LetsMeetBot();
             api.registerBot(letsMeetBot);
-        } catch (IOException e) {
-            System.out.println("Current dir is " + System.getProperty("user.dir"));
-            e.printStackTrace();
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
